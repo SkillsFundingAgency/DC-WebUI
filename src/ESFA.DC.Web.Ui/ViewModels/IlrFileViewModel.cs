a52b35296dbcdc426e98e3a5b59f0c2a8f396015
Salman Baig <salman.sfa@outlook.com>
2018-03-28 10:50:54 +0100

    changes for date time format

--- src/ESFA.DC.Web.Ui/ViewModels/IlrFileViewModel.cs
+++ src/ESFA.DC.Web.Ui/ViewModels/IlrFileViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace DC.Web.Ui.ViewModels
 {
@@ -6,6 +7,8 @@ namespace DC.Web.Ui.ViewModels
     {
         public string Filename;
         public Guid CorrelationId;
+
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy hh:mm:ss.fff tt}")]
         public DateTime SubmissionDateTime { get; set; }
         public decimal FileSize { get; set; }
     }
