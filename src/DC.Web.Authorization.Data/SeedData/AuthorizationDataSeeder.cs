1c32189b32615de99196e1645e6c5b3a2c759758
Salman Baig <salman.sfa@outlook.com>
2018-04-09 15:40:40 +0100

    data seeder tests

--- src/DC.Web.Authorization.Data/SeedData/AuthorizationDataSeeder.cs
+++ src/DC.Web.Authorization.Data/SeedData/AuthorizationDataSeeder.cs
@@ -10,26 +10,34 @@ namespace DC.Web.Authorization.Data.SeedData
         {
             context.Database.EnsureCreated();
 
-            // Look for any students.
+            // Look for any roles
             if (context.Roles.Any())
             {
                 return;   // DB has been seeded
             }
 
-            var roles = new Role[]
+            AddRoles(context);
+            AddFeatures(context);
+            AddRoleFeatures(context);
+        }
+
+        public static void AddRoleFeatures(AuthorizeDbContext context)
+        {
+            var roleFeatures = new RoleFeature[]
             {
-                new Role { Id = 1, Name = RoleNames.DAA, Description = "Data Collections Admin" },
-                new Role { Id = 2, Name = RoleNames.DCS, Description = "Data Collections Support" },
-                new Role { Id = 3, Name = RoleNames.BI, Description = "Reports and BI" },
-                new Role { Id = 4, Name = RoleNames.DCI, Description = "Data Collections Information Officer" },
+                new RoleFeature { FeatureId = 1, RoleId = 1 },
+                new RoleFeature { FeatureId = 2, RoleId = 1 },
             };
-            foreach (var role in roles)
+            foreach (var rp in roleFeatures)
             {
-                context.Roles.Add(role);
+                context.RoleFeatures.Add(rp);
             }
 
             context.SaveChanges();
+        }
 
+        public static void AddFeatures(AuthorizeDbContext context)
+        {
             var features = new Feature[]
             {
                 new Feature() { Id = 1, Name = FeatureNames.FileSubmission, Description = "File Submission" },
@@ -41,15 +49,20 @@ namespace DC.Web.Authorization.Data.SeedData
             }
 
             context.SaveChanges();
+        }
 
-            var roleFeatures = new RoleFeature[]
+        public static void AddRoles(AuthorizeDbContext context)
+        {
+            var roles = new Role[]
             {
-                new RoleFeature { FeatureId = 1, RoleId = 1 },
-                new RoleFeature { FeatureId = 2, RoleId = 1 },
+                new Role { Id = 1, Name = RoleNames.DAA, Description = "Data Collections Admin" },
+                new Role { Id = 2, Name = RoleNames.DCS, Description = "Data Collections Support" },
+                new Role { Id = 3, Name = RoleNames.BI, Description = "Reports and BI" },
+                new Role { Id = 4, Name = RoleNames.DCI, Description = "Data Collections Information Officer" },
             };
-            foreach (var rp in roleFeatures)
+            foreach (var role in roles)
             {
-                context.RoleFeatures.Add(rp);
+                context.Roles.Add(role);
             }
 
             context.SaveChanges();
