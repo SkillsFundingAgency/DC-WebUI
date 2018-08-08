72d0a5c5b8edf2685c421798d57ac3867575e58b
Salman Baig <salman.sfa@outlook.com>
2018-08-08 16:27:32 +0100

    removed not needed references

--- src/ESFA.DC.Web.Ui.ViewModels/IlrSubmissionConfirmationViewModel.cs
+++ src/ESFA.DC.Web.Ui.ViewModels/IlrSubmissionConfirmationViewModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Text;
-
-namespace ESFA.DC.Web.Ui.ViewModels
+﻿namespace ESFA.DC.Web.Ui.ViewModels
 {
     public class IlrSubmissionConfirmationViewModel
     {
