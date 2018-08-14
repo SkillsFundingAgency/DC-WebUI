c18623769b7feb837d60613efc30a77334655dc6
Salman Baig <salman.sfa@outlook.com>
2018-08-14 13:06:49 +0100

    added error handling summary and tag helpers

---
+++ src/ESFA.DC.Web.Ui/TagHelpers/ErrorStyleTagHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace DC.Web.Ui.TagHelpers
+{
+    [HtmlTargetElement("div", Attributes = "asp-valid")]
+    public class ErrorStyleTagHelper : TagHelper
+    {
+        [HtmlAttributeName("asp-valid")]
+        public bool IsValid { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var existingClass = output.Attributes.FirstOrDefault(a => a.Name == "class")?.Value;
+            if (!IsValid)
+            {
+                output.Attributes.SetAttribute("class", $"{existingClass} govuk-form-group--error");
+            }
+        }
+    }
+}
