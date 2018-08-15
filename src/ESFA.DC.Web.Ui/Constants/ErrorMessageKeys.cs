7508e82d1d627e54282888f4e8ac176b705422d3
Salman Baig <salman.sfa@outlook.com>
2018-08-15 16:18:28 +0100

    changes for options page to add errors and display options

---
+++ src/ESFA.DC.Web.Ui/Constants/ErrorMessageKeys.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DC.Web.Ui.Constants
+{
+    public static class ErrorMessageKeys
+    {
+        public const string ErrorSummaryKey = "Summary";
+        public const string SubmissionOptions_OptionsFieldKey = "Options";
+    }
+}
---
+++ src/ESFA.DC.Web.Ui/Constants/ErrorMessageLookup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DC.Web.Ui.Constants
+{
+    public static class ErrorMessageLookup
+    {
+        private static readonly IReadOnlyDictionary<string, string> MessagesDictionary =
+            new Dictionary<string, string>()
+            {
+                { ErrorMessageKeys.SubmissionOptions_OptionsFieldKey, "Choose an option from the list" }
+            };
+
+        public static string GetErrorMessage(string key)
+        {
+            return MessagesDictionary.ContainsKey(key) ? MessagesDictionary[key] : string.Empty;
+        }
+    }
+}
