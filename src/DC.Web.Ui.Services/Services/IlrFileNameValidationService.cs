78f8fca3d925380154fde08b1d6c07332165aab9
Salman Baig <salman.sfa@outlook.com>
2018-10-22 16:50:51 +0100

    #61142,  #61140, #60564 ui bugs

--- src/DC.Web.Ui.Services/Services/IlrFileNameValidationService.cs
+++ src/DC.Web.Ui.Services/Services/IlrFileNameValidationService.cs
@@ -15,7 +15,7 @@ namespace DC.Web.Ui.Services.Services
     public class IlrFileNameValidationService : IFileNameValidationService
     {
         private readonly IKeyValuePersistenceService _persistenceService;
-        private readonly Regex _fileNameRegex = new Regex("^(ILR)-([1-9][0-9]{7})-(1819)-((20[0-9]{2})(0[1-9]|1[012])([123]0|[012][1-9]|31))-(([01][0-9]|2[0-3])([0-5][0-9])([0-5][0-9]))-(([1-9][0-9])|(0[1-9])).((XML)|(ZIP)|(xml)|(zip))$", RegexOptions.Compiled);
+        private readonly Regex _fileNameRegex = new Regex("^(ILR)-([1-9][0-9]{7})-([1-9]{4})-((20[0-9]{2})(0[1-9]|1[012])([123]0|[012][1-9]|31))-(([01][0-9]|2[0-3])([0-5][0-9])([0-5][0-9]))-([0-9]{2}).((XML)|(ZIP)|(xml)|(zip))$", RegexOptions.Compiled);
 
         public IlrFileNameValidationService([KeyFilter(JobType.IlrSubmission)]IKeyValuePersistenceService persistenceService)
         {
@@ -45,6 +45,26 @@ namespace DC.Web.Ui.Services.Services
                 };
             }
 
+            if (!IsValidYear(fileName))
+            {
+                return new FileNameValidationResultViewModel()
+                {
+                    ValidationResult = FileNameValidationResult.InvalidYear,
+                    FieldError = "The year in the filename should match the current year",
+                    SummaryError = "The year in the filename should match the current year"
+                };
+            }
+
+            if (!IsValidSerialNumber(fileName))
+            {
+                return new FileNameValidationResultViewModel()
+                {
+                    ValidationResult = FileNameValidationResult.InvalidSerialNumber,
+                    FieldError = "Serial number in the filename must not be 00",
+                    SummaryError = "Serial number in the filename must not be 00"
+                };
+            }
+
             if (!IsValidRegex(fileName))
             {
                 var fileExtension = fileName.Substring(fileName.Length - 4);
@@ -63,16 +83,6 @@ namespace DC.Web.Ui.Services.Services
                     ValidationResult = FileNameValidationResult.InvalidFileNameFormat,
                     FieldError = "The UKPRN in the filename does not match the UKPRN associated with your IdAMS account",
                     SummaryError = "The UKPRN in the filename does not match the UKPRN associated with your IdAMS account"
-                };
-            }
-
-            if (!IsValidYear(fileName))
-            {
-                return new FileNameValidationResultViewModel()
-                {
-                    ValidationResult = FileNameValidationResult.InvalidYear,
-                    FieldError = "The year in the filename should match the current year",
-                    SummaryError = "The year in the filename should match the current year"
                 };
             }
 
@@ -101,15 +111,35 @@ namespace DC.Web.Ui.Services.Services
         public bool IsValidUkprn(string fileName, long ukprn)
         {
             var matches = _fileNameRegex.Match(fileName);
-            long.TryParse(matches.Groups[2].Value, out var fileUkprn);
+            long fileUkprn = 0;
+            if (matches.Groups.Count > 2)
+            {
+                long.TryParse(matches.Groups[2].Value, out fileUkprn);
+            }
+
             return fileUkprn == ukprn;
+        }
+
+        public bool IsValidSerialNumber(string fileName)
+        {
+            if (!IsValidRegex(fileName))
+            {
+                return true;
+            }
+
+            var matches = _fileNameRegex.Match(fileName);
+            return matches.Groups[12].Value != "00";
         }
 
         public bool IsValidYear(string fileName)
         {
+            if (!IsValidRegex(fileName))
+            {
+                return true;
+            }
+
             var matches = _fileNameRegex.Match(fileName);
-            int.TryParse(matches.Groups[5].Value, out var year);
-            return year == DateTime.Now.Year;
+            return matches.Groups[3].Value == "1819";
         }
 
         public async Task<bool> IsUniqueFileAsync(string fileName)
