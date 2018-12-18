e9828ab439a8e6e6d6fe12bcdf6cb8684a81f2d8
Salman Baig <salman.sfa@outlook.com>
2018-12-18 16:15:05 +0000

    AB#65938 return window widget

---
+++ src/ESFA.DC.Web.Ui/Areas/Helpdesk/ViewComponents/ReturnWindowViewComponent.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DC.Web.Ui.Constants;
+using DC.Web.Ui.Services.Extensions;
+using DC.Web.Ui.Services.Interfaces;
+using ESFA.DC.CollectionsManagement.Models;
+using ESFA.DC.DateTimeProvider.Interface;
+using ESFA.DC.Web.Ui.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DC.Web.Ui.Areas.Helpdesk.ViewComponents
+{
+    public class ReturnWindowViewComponent : ViewComponent
+    {
+        private readonly ICollectionManagementService _collectionManagementService;
+        private readonly IDateTimeProvider _dateTimeProvider;
+
+        public ReturnWindowViewComponent(ICollectionManagementService collectionManagementService, IDateTimeProvider dateTimeProvider)
+        {
+            _collectionManagementService = collectionManagementService;
+            _dateTimeProvider = dateTimeProvider;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var collection = await _collectionManagementService.GetCollectionFromTypeAsync(CollectionTypes.ILR);
+
+            if (collection != null)
+            {
+                var currentPeriod =
+                    await _collectionManagementService.GetPeriodAsync(collection.CollectionTitle, _dateTimeProvider.GetNowUtc());
+
+                if (currentPeriod != null)
+                {
+                    var returnWindow = new ReturnPeriodViewModel(currentPeriod.PeriodNumber)
+                    {
+                        DaysToClose = (currentPeriod.EndDateTimeUtc - _dateTimeProvider.GetNowUtc()).Days,
+                        PeriodCloseDate = currentPeriod.EndDateTimeUtc.ToDateTimeDisplayFormat()
+                    };
+
+                    return View(returnWindow);
+                }
+            }
+
+            return View(new ReturnPeriodViewModel(0));
+        }
+    }
+}
